c295acf8750ce4c3dcde9ce93b8e49e3dff26147
Yusuf Zain Elabdin <130767740+Yusuf0Zain@users.noreply.github.com>
2024-06-06 18:44:27 +0800

    update

--- BounceBall/scripts/GameMan.gd
+++ BounceBall/scripts/GameMan.gd
@@ -1,23 +1,34 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/Level2.tscn")
-
-
+	get_tree().change_scene_to_file("res://scenes/Level1.tscn")
 func _on_levels_pressed():
-	pass # Replace with function body.
-
-
+	$Lgoo.hide()
+	$Levels.show()
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://scenes/nokiaMain.tscn")
+
+
+
+
+func _on_one_pressed():
+	get_tree().change_scene_to_file("res://scenes/Level1.tscn")
+
+
+func _on_two_pressed():
+	get_tree().change_scene_to_file("res://scenes/level2.tscn")
+
+
+func _on_three_pressed():
+	get_tree().change_scene_to_file("res://scenes/Level3.tscn")
+
+func _on_boss_pressed():
+	get_tree().change_scene_to_file("res://scenes/BossLevel.tscn")
+
+func _on_secret_pressed():
+	get_tree().change_scene_to_file("res://scenes/Secret.tscn")
+
+
+func _on_back_menu_pressed():
+	$Lgoo.show()
+	$Levels.hide()
--- BounceBall/scripts/ball.gd
+++ BounceBall/scripts/ball.gd
@@ -1,48 +1,75 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 400.0
+const MAX_SPEED = 200.0
 const MIN_SPEED = 50.0
 const ACCELERATION_TIME = 2.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -500.0
 const FIRST_BOUNCE_VELOCITY = -200.0
-const SECOND_BOUNCE_VELOCITY = -60.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var current_speed = MIN_SPEED
 var acceleration = (MAX_SPEED - MIN_SPEED) / ACCELERATION_TIME
-var was_on_floor = false  # Tracks whether the ball was on the floor in the previous frame
-var bounce_count = 0  # Counts the number of bounces
+var was_on_floor = false
+var idle_time = 0.0
+var IDLE_THRESHOLD = 5.0
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var timer = $Timer
+
+enum AnimationState { IDLE, RUN, JUMP, FALL, WAIT, FLOOR }
+var current_animation_state = AnimationState.IDLE
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle jump.
+		set_animation_state(AnimationState.FALL)
+	else:
+		set_animation_state(AnimationState.IDLE)
+	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		bounce_count = 0  # Reset bounce count when jumping
+		set_animation_state(AnimationState.JUMP)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction != 0:
 		current_speed = min(current_speed + acceleration * delta, MAX_SPEED)
 		velocity.x = direction * current_speed
+		idle_time = 0.0
+		if current_animation_state != AnimationState.JUMP:
+			set_animation_state(AnimationState.RUN)
 	else:
 		current_speed = MIN_SPEED
 		velocity.x = move_toward(velocity.x, 0, acceleration * delta)
+		if is_on_floor() and velocity.x == 0:
+			idle_time += delta
+			if idle_time >= IDLE_THRESHOLD:
+				set_animation_state(AnimationState.WAIT)
+			elif current_animation_state != AnimationState.WAIT:
+				set_animation_state(AnimationState.IDLE)
 
-	# Apply bounce effect when landing
 	if not was_on_floor and is_on_floor():
-		if bounce_count == 0:
-			velocity.y = FIRST_BOUNCE_VELOCITY
-			bounce_count += 1
-		elif bounce_count == 1:
-			velocity.y = SECOND_BOUNCE_VELOCITY
-			bounce_count += 1
-
+		set_animation_state(AnimationState.FLOOR)
+		idle_time = 0.0
 	was_on_floor = is_on_floor()
 
 	move_and_slide()
+
+func set_animation_state(state):
+	if state != current_animation_state:
+		current_animation_state = state
+		match state:
+			AnimationState.IDLE:
+				animated_sprite.play("idle")
+			AnimationState.RUN:
+				animated_sprite.play("run")
+			AnimationState.JUMP:
+				animated_sprite.play("jump")
+			AnimationState.FALL:
+				animated_sprite.play("fall")
+			AnimationState.WAIT:
+				animated_sprite.play("wait")
+			AnimationState.FLOOR:
+				animated_sprite.play("floor")
+
+func _on_AnimatedSprite2D_animation_finished():
+	if current_animation_state == AnimationState.FLOOR:
+		set_animation_state(AnimationState.IDLE)
